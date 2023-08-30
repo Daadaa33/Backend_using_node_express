6d1b2faf3822c60469052a4c78b470c8a85a77c1
daadaa <wlaaldaadaa33@gmail.com>
2023-08-30 10:26:21 +0300

    --save dotenv

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-require('dotenv').config();
+require('dotenv').config()
 // require your server and launch it
 const server = require('./api/server');
 
 const PORT = process.env.PORT || 4000;
 
+console.log("env from render is ",process.env.PORT)
+
 server.listen(PORT, () => {
-    console.log(`\n***** server Running on http://localhost:${PORT} ****\n`)
+    console.log(`\n***** your server Running on http://localhost:${PORT} ****\n`)
 })
